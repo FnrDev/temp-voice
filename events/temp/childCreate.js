d47cf9f8496706871b0ac622287ec81556fe5fae
Fnr <ah2014bh@outlook.com>
2022-04-27 04:29:33 +0300

    fix logger issue

--- events/temp/childCreate.js
+++ events/temp/childCreate.js
@@ -26,21 +26,8 @@ module.exports = async(client, member, channel) => {
     const logChannel = member.guild.channels.cache.get(log_channel_id);
     if (!logChannel) return console.error(`[Temp Channel] i can't find log channel with ${log_channel_id} id`);
 
-    // fetch all webhooks from log channel and filter webhooks created by the bot
-    const fetchWebhooks = await logChannel.fetchWebhooks();
-    const filterdWebhooks = fetchWebhooks.filter(r => r.owner.id === client.user.id);
-
-    // check if there non webhooks created by the bot
-    if (!filterdWebhooks.size) {
-        // create webhook for log channel
-        const webhookData = await logChannel.createWebhook('Temp Channel Logger', { reason: "Temp channels logger" });
-
-        // create webhook client
-        const webhook = new WebhookClient({ url: webhookData.url });
-
-        // send log using webhook
-        webhook.send({
-            content: `✅ Created temp channel for ${member.user.tag} (\`${member.id}\`) `
-        })
-    }
+    // send log to channel
+    logChannel.send({
+        content: `✅ Done! created temp channel for ${member.user.tag} (${member.id})`
+    })
 }
